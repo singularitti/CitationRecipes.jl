bd7b3dae6544915b86e800aebf30506ad7f4dedd
singularitti <singularitti@outlook.com>
2023-03-06 00:47:00 -0500

    Add function `experimental`, `theoretical` & `truncate_authors`

--- src/CitationRecipes.jl
+++ src/CitationRecipes.jl
@@ -1,5 +1,39 @@
 module CitationRecipes
 
-# Write your package code here.
+using Bibliography: Entry, xyear
+using RecipesBase: AbstractPlot, @recipe, @series, @shorthands
+
+@recipe function f(::Type{Val{:experimental}}, plt::AbstractPlot; ref=nothing, n_authors=1)
+    label = if ref isa Entry
+        join((truncate_authors(ref, n_authors), xyear(ref)), ", ")
+    else
+        string(get(plotattributes, :label, ""))
+    end
+    seriestype := :scatter  # It has to be `:=`!
+    markersize --> 3
+    markerstrokecolor --> :auto
+    markerstrokewidth --> 0
+    label --> label
+    return ()
+end
+@shorthands experimental
+
+@recipe function f(::Type{Val{:theoretical}}, plt::AbstractPlot; ref=nothing, n_authors=1)
+    label = if ref isa Entry
+        join((truncate_authors(ref, n_authors), xyear(ref)), ", ")
+    else
+        string(get(plotattributes, :label, ""))
+    end
+    seriestype := :path  # It has to be `:=`!
+    label --> label
+    return ()
+end
+@shorthands theoretical
+
+function truncate_authors(ref::Entry, n::Integer)
+    authors = Iterators.take(ref.authors, n)
+    suffix = n < length(ref.authors) ? "et. al." : ""
+    return join([[author.last for author in authors]; suffix], ", ")
+end
 
 end
